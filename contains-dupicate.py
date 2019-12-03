b4951fca25c0b9f1777aaa65a3b031f13e36c2db
Manikandan R <vishnumani.2009@rediffmail.com>
2019-12-03 06:17:52 +0530

    Contains duplicated in an array

---
+++ contains-dupicate.py
@@ -0,0 +1,42 @@
+"""
+217. Contains Duplicate
+Easy
+
+Given an array of integers, find if the array contains any duplicates.
+
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: true
+Example 2:
+
+Input: [1,2,3,4]
+Output: false
+Example 3:
+
+Input: [1,1,1,3,3,4,3,2,4,2]
+Output: true
+"""
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        hashmap={}
+        for index in range(0,len(nums)):
+            hashmap[nums[index]]=0
+            
+        for index in range(0,len(nums)):
+            target = nums[index]
+            
+            if hashmap[target]==1:
+                return True
+            else:
+                hashmap[target]=1
+        return False
+            
+        
